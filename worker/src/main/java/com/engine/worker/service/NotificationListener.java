ac67f71c33d79122a92bb60445993a3154e4f0c1
Prathamesh Pawar <prathmeshpawar4169@gmail.com>
2026-03-03 16:25:19 +0530

    Added proper logs in worker module

--- worker/src/main/java/com/engine/worker/service/NotificationListener.java
+++ worker/src/main/java/com/engine/worker/service/NotificationListener.java
@@ -2,17 +2,23 @@ package com.engine.worker.service;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 import common.constants.AppConstants;
 import common.dto.NotificationDTO;
 
-import java.util.List;
-
 @Component
+@Slf4j
 public class NotificationListener {
 
     @RabbitListener(queues = AppConstants.QUEUE_NAME)
-    public void consume(Object message) {
-        System.out.println("Received → " + message);
-        System.out.println("Received Single → " + message);
+    public void consume(NotificationDTO notification) {
+
+        log.info("[WORKER] Received → recipient={}", notification.recipient());
+
+        log.info("[WORKER] Processing → type={}, recipient={}",
+                notification.providerType(),
+                notification.recipient());
+
+        log.info("[WORKER] Completed → recipient={}", notification.recipient());
     }
 }
